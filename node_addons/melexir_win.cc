e87175fb6bbec150d1e5a3e2e8bb1884e2dbeb45
= <=>
2022-04-29 14:12:25 -0700

    Got it working!

--- node_addons/melexir_win.cc
+++ node_addons/melexir_win.cc
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <string>
 #include <napi.h>
+#include "timer.h"
 #include "mlxr.h"
 
 using namespace std;
@@ -12,19 +13,20 @@ using namespace std;
 extern "C" {
     void setoptions_(char optString[], long int strLength);
     void checkoption_(char key[], int* iopt, double* fopt, bool* qopt, long int keyLength);
-    void image2data_(double fimage[][50], int* ldf, int* nrow, int* ncol, double dat[], int* ldd);
+    //void image2data_(double fimage[][50], int* ldf, int* nrow, int* ncol, double dat[], int* ldd);
+    void image2data_(double fimage[], int* ldf, int* nrow, int* ncol, double dat[], int* ldd);
     void melexirdll_(double dat[], double sigma[], double fmap[], double base[], double datainv[], int* nr, int* nt);
 }
 
-double image[50][50];
+double image[1024][1024];
+int image_width = 1024;
+int image_height = 1024;
 // Initialize image with 1 transition
 void InitImage(const Napi::CallbackInfo& info) {
-    /*srand(time(NULL)); // Setting up random number generator
+    Timer sim_time;
+    srand(time(NULL)); // Setting up random number generator
 
     float const pi = 3.14159265358979;
-
-    int image_width = 50;
-    int image_height = 50;
 
     // First make sure image is blank
     for (int Y = 0; Y < image_height; Y++) {
@@ -34,7 +36,7 @@ void InitImage(const Napi::CallbackInfo& info) {
     }
 
     // Then add electrons
-    int radius = 15;
+    int radius = 150;
     int center_x = image_width / 2; int center_y = image_height / 2;
     long int e_count = 100000;
     long int total_count = 0;
@@ -48,7 +50,9 @@ void InitImage(const Napi::CallbackInfo& info) {
 		int Y = round(center_y + radius * cos(theta));
         image[Y][X]++;
         total_count++;
-    }*/
+    }
+
+    sim_time.endPrint("Time to simulate");
 
     /*// Print the image
     for (int Y = 0; Y < image_height; Y++) {
@@ -60,35 +64,56 @@ void InitImage(const Napi::CallbackInfo& info) {
     cout << endl;*/
 
     // Use image from file instead
-    getImageFromArr(image);
+    //getImageFromArr(image);
 }
 
 void Test(const Napi::CallbackInfo& info) {
-    char string1[] = "test_image.dat -LP2";
+    Timer overall_time;
+    char string1[] = "-LP2 -MC0 -IX512 -IZ512";
 
     setoptions_(string1, sizeof(string1));
     // Need to end the string with a null character since Fortran doesn't (but C++ requires it)
     string1[sizeof(string1)-1] = '\0';
 
-    // Get the number of Legendre components (will be i + 1)
-    char k[]  = "L"; int i = 1; double f; bool q;
-    checkoption_(k, &i, &f, &q, sizeof(k));
-    int nl = i;
-    cout << "nl: " << nl << endl;
+    // Get the number of Legendre components (will fill iopt)
+    char key[]  = "L"; int iopt; double fopt; bool qopt;
+    int nl; int nl_even; int nl_odd; // Total Legendre components, and even and odd portions
+    checkoption_(key, &iopt, &fopt, &qopt, sizeof(key));
+    // Figure out even and odd components (logic taken from melexir.f90 ln83)
+    if (iopt >= 10) {
+        // => Two digits, must have even and odd components
+        nl_even = iopt / 10; // First digit is even components
+        nl_odd = iopt % 10; // Second digit is odd components
+    } else {
+        // Only one digit => only even components
+        nl_even = iopt;
+        nl_odd = 0;
+    }
+    nl_even = nl_even / 2 + 1; // Get count of just even components (including zero)
+    nl_odd = (nl_odd + 1) / 2; // Get count of just odd components (0.5 rounds down to 0 bc it's an int)
+    nl = nl_even + nl_odd;
+
+    Timer time;
+    double* flat_image = new double[image_height*image_width];
+    // Flatten into a column-major 1D array
+    for (int row = 0; row < image_height; row++) {
+        for (int col = 0; col < image_width; col++) {
+            flat_image[image_width*row + col] = image[col][row]; // Need to double check this
+        }
+    } 
+    time.endPrint("Time to flatten");
 
     // Prepare image for MELEXIR
-    int nrow = 50;
-    int ncol = 50;
-    int ldd = pow(max(nrow, ncol),2); // Comes from PrepareVMI3.f90
+    int nrow = image_height;
+    int ncol = image_width;
+    int ldd = pow(max(nrow, ncol),2); // Largest possible value for length of contracted data (Comes from PrepareVMI3.f90 ln104)
     double* dat = new double[ldd];
-    image2data_(image, &nrow, &nrow, &ncol, dat, &ldd);
-
-    cout << "pixel before: " << image[10][25] << endl;
+    //image2data_(image, &nrow, &nrow, &ncol, dat, &ldd);
+    image2data_(flat_image, &nrow, &nrow, &ncol, dat, &ldd);
 
     // Run MELEXIR
     //int nt = nrow * ncol;
     int nt = nrow * nl;
-    cout << nt << ", " << nrow << ", " << ncol << endl;
     // Allocate memory for input/output arrays
     double* dat2 = new double[nt]; // Legendre-projected data
     double* sigma = new double[nt]; // Residuals
@@ -117,21 +142,7 @@ void Test(const Napi::CallbackInfo& info) {
     // sigma will be the spectrum
     // dat will be the residuals (idk why he swaps it)
 
-    cout << "pixel after: " << image[10][25] << endl;
-    
-    for (int i = 0; i < nrow; i++) {
-        cout << sigma[i] << ",";
-    }
-    cout << endl << endl;
-    for (int i = nrow; i < 2*nrow; i++) {
-        cout << sigma[i] << ",";
-    }
-    cout << endl << endl;
-    for (int i = 2*nrow; i < 3*nrow; i++) {
-        cout << sigma[i] << ",";
-    }
-    cout << endl << endl;
-    cout << "Done" << endl;
+    overall_time.endPrint("Time to complete");
 
 }
 
