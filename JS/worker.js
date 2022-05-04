82b378c44382f2f064c4bcc5286529ed10953fc7
Marby OldMac <martydewitt2015@gmail.com>
2022-05-03 17:39:24 -0700

    Added "self."

--- JS/worker.js
+++ JS/worker.js
@@ -7,8 +7,8 @@ const null_results = {
 }
 
 // Receive message from Main Window
-onmessage = function(e) {
-    let image = e.data;
+self.onmessage = function(event) {
+    let image = event.data;
     // Need to make sure it's a 2D array
     // (Didn't do this in a single if statement bc if it's a 1D or 0D array, 
     //      image[0][0] would throw an error)
@@ -17,21 +17,22 @@ onmessage = function(e) {
             if (Array.isArray(image[0][0])) {
                 // Dimension is larger than 2D
                 console.log("MELEXIR Worker: image needs to be a 2D array - image is too large");
-                postMessage(null_results);
+                self.postMessage(null_results);
             } else {
                 // It is a 2D array
                 let results = melexir.process(image);
                 console.log("MELEXIR Worker: Successfully process image");
-                postMessage(results);
+                self.postMessage(results);
             }
         } else {
             // Array is only 1D
             console.log("MELEXIR Worker: image needs to be a 2D array - image is only 1D array");
-            postMessage(null_results);
+            self.postMessage(null_results);
         }
     } else {
         // image is not an array
         console.log("MELEXIR Worker: image needs to be a 2D array - image is not an array");
-        postMessage(null_results);
+        self.postMessage(null_results);
+        self.close();
     }
 }
