240e61832389fd63b6e073ad8642b3b2dddbd60c
Marby <martydewitt2015@gmail.com>
2023-07-15 13:40:44 -0700

    Ease of readability changes

--- JS/MainWindow/wavelengthClasses.js
+++ JS/MainWindow/wavelengthClasses.js
@@ -7,7 +7,7 @@ const { LASER } = require("../Messages.js");
 **************************************************/
 
 // Essentially a fancier version of {wavelength: val, wavenumber: val}
-class LaserMode {
+class LaserModeEnergy {
 	constructor(instance, mode) {
 		this.instance = instance;
 		this.mode = mode;
@@ -56,12 +56,12 @@ class LaserMode {
 
 class DetachmentWavelength {
 	constructor() {
-		let this_instance = this; // Need to do this so LaserMode methods can get access to DetachmentWavelength class methods
+		let this_instance = this; // Need to do this so LaserModeEnergy methods can get access to DetachmentWavelength class methods
 
-		this.standard = new LaserMode(this_instance, this.MODE.STANDARD);
-		this.doubled = new LaserMode(this_instance, this.MODE.DOUBLED);
-		this.raman = new LaserMode(this_instance, this.MODE.RAMAN);
-		this.irdfg = new LaserMode(this_instance, this.MODE.IRDFG);
+		this.standard = new LaserModeEnergy(this_instance, this.MODE.STANDARD);
+		this.doubled = new LaserModeEnergy(this_instance, this.MODE.DOUBLED);
+		this.raman = new LaserModeEnergy(this_instance, this.MODE.RAMAN);
+		this.irdfg = new LaserModeEnergy(this_instance, this.MODE.IRDFG);
 	}
 
 	/* Static Methods */
@@ -133,19 +133,19 @@ class DetachmentWavelength {
 		this.irdfg.reset();
 	}
 
-	convert(laser_mode) {
-		switch (laser_mode.mode) {
+	convert(laser_mode_energy) {
+		switch (laser_mode_energy.mode) {
 			case this.MODE.STANDARD:
-				DetachmentWavelength_convert_standard(this, laser_mode);
+				DetachmentWavelength_convert_standard(this, laser_mode_energy);
 				break;
 			case this.MODE.DOUBLED:
-				DetachmentWavelength_convert_doubled(this, laser_mode);
+				DetachmentWavelength_convert_doubled(this, laser_mode_energy);
 				break;
 			case this.MODE.RAMAN:
-				DetachmentWavelength_convert_raman(this, laser_mode);
+				DetachmentWavelength_convert_raman(this, laser_mode_energy);
 				break;
 			case this.MODE.IRDFG:
-				DetachmentWavelength_convert_irdfg(this, laser_mode);
+				DetachmentWavelength_convert_irdfg(this, laser_mode_energy);
 				break;
 		}
 	}
@@ -218,12 +218,12 @@ function DetachmentWavelength_convert_irdfg(instance, irdfg_energy) {
 
 class ExcitationWavelength {
 	constructor() {
-		let this_instance = this; // Need to do this so LaserMode methods can get access to ExcitationWavelength class methods
+		let this_instance = this; // Need to do this so LaserModeEnergy methods can get access to ExcitationWavelength class methods
 
-		this.nIR = new LaserMode(this_instance, this.MODE.NIR);
-		this.iIR = new LaserMode(this_instance, this.MODE.IIR);
-		this.mIR = new LaserMode(this_instance, this.MODE.MIR);
-		this.fIR = new LaserMode(this_instance, this.MODE.FIR);
+		this.nIR = new LaserModeEnergy(this_instance, this.MODE.NIR);
+		this.iIR = new LaserModeEnergy(this_instance, this.MODE.IIR);
+		this.mIR = new LaserModeEnergy(this_instance, this.MODE.MIR);
+		this.fIR = new LaserModeEnergy(this_instance, this.MODE.FIR);
 	}
 
 	/* Static Methods */
@@ -334,19 +334,19 @@ class ExcitationWavelength {
 		this.fIR.reset();
 	}
 
-	convert(laser_mode) {
-		switch (laser_mode.mode) {
+	convert(laser_mode_energy) {
+		switch (laser_mode_energy.mode) {
 			case this.MODE.NIR:
-				ExcitationWavelength_convert_nir(this, laser_mode);
+				ExcitationWavelength_convert_nir(this, laser_mode_energy);
 				break;
 			case this.MODE.IIR:
-				ExcitationWavelength_convert_iir(this, laser_mode);
+				ExcitationWavelength_convert_iir(this, laser_mode_energy);
 				break;
 			case this.MODE.MIR:
-				ExcitationWavelength_convert_mir(this, laser_mode);
+				ExcitationWavelength_convert_mir(this, laser_mode_energy);
 				break;
 			case this.MODE.FIR:
-				ExcitationWavelength_convert_fir(this, laser_mode);
+				ExcitationWavelength_convert_fir(this, laser_mode_energy);
 				break;
 		}
 	}
@@ -356,6 +356,7 @@ class ExcitationWavelength {
 		ExcitationWavelength Class Functions
 ***/
 
+// Convert nIR energies to iIR, mIR, and fIR
 function ExcitationWavelength_convert_nir(instance, nir_energy) {
 	// (Need to edit underscored version (e.g. nIR._wavenumber) so that conversions aren't retriggered)
 	// First working in wavenumbers since the math is easier
@@ -368,6 +369,7 @@ function ExcitationWavelength_convert_nir(instance, nir_energy) {
 	instance.fIR._wavelength = 1e7 / instance.fIR._wavenumber;
 }
 
+// Convert iIR energies to nIR, mIR, and fIR
 function ExcitationWavelength_convert_iir(instance, iir_energy) {
 	// (Need to edit underscored version (e.g. nIR._wavenumber) so that conversions aren't retriggered)
 	// First working in wavenumbers since the math is easier
@@ -380,6 +382,7 @@ function ExcitationWavelength_convert_iir(instance, iir_energy) {
 	instance.fIR._wavelength = 1e7 / instance.fIR._wavenumber;
 }
 
+// Convert mIR energies to nIR, iIR, and fIR
 function ExcitationWavelength_convert_mir(instance, mir_energy) {
 	// (Need to edit underscored version (e.g. nIR._wavenumber) so that conversions aren't retriggered)
 	// First working in wavenumbers since the math is easier
@@ -392,6 +395,7 @@ function ExcitationWavelength_convert_mir(instance, mir_energy) {
 	instance.fIR._wavelength = 1e7 / instance.fIR._wavenumber;
 }
 
+// Convert fIR energies to nIR, iIR, and mIR
 function ExcitationWavelength_convert_fir(instance, fir_energy) {
 	// (Need to edit underscored version (e.g. nIR._wavenumber) so that conversions aren't retriggered)
 	// First working in wavenumbers since the math is easier
